258a15312a96ff4086a3a24817124572c9a8550f
Stephen Tudor <smt@sunblu.sh>
2011-07-26 23:12:14 -0400

    Add fresh rails app

---
+++ elcheapo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elcheapo::Application.config.secret_token = 'a358bdacaf14f0db31acdf74e6f1fe90e94b1250c4bb6a73a458621ce001c96ed123d06d9090ef4cf8710a5bc1ea10580451ea659f26ddeba96792451c381c76'
